77d8ccf2fe302281c4bcf53771843df908eea810
rheamach <makhijarhea2012@gmail.com>
2024-12-25 03:55:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,8 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your smoothie will be: ", name_on_order)
 
-session = get_active_session()
+session = Session.builder.config("connection_name", "myconnection").create()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
